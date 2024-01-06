31468dfef04b88d5f2be422a822e0094563bfbcf
noahra <noah.rahimzadagan@protonmail.com>
2024-01-06 19:57:59 +0100

    Refactoring finished

--- src/ui.rs
+++ src/ui.rs
@@ -49,119 +49,7 @@ pub fn create_display_list<T: std::fmt::Display>(
         .map(|(index, n)| format!("{:width$}. {}", index + 1, n, width = max_index_width))
         .collect()
 }
-fn ui(app: &mut App, f: &mut Frame) {
-    let constraints = [
-        Constraint::Percentage(5),
-        Constraint::Percentage(50),
-        Constraint::Percentage(5),
-        Constraint::Percentage(5),
-        Constraint::Percentage(5),
-        Constraint::Percentage(5),
-        Constraint::Percentage(5),
-    ];
 
-    let layout = Layout::default()
-        .constraints(constraints.as_ref())
-        .split(f.size());
-
-    let converted_values;
-
-    match app.current_format {
-        Format::Utf8 => {
-            converted_values = create_display_list(&app.converted_binary_to_utf8.clone(), app);
-        }
-        Format::Uint32 => {
-            converted_values = create_display_list(&app.converted_numbers.clone(), app);
-        }
-    }
-
-    let list = List::new(converted_values)
-        .block(
-            Block::default()
-                .title("Converted binary values")
-                .borders(Borders::ALL),
-        )
-        .style(Style::default().fg(Color::Green))
-        .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
-        .highlight_symbol(">>")
-        .repeat_highlight_symbol(true);
-
-    let current_format_paragraph = Paragraph::new(Text::raw(format!(
-        "Current Format: {:?}",
-        app.current_format
-    )))
-    .style(Style::default().fg(Color::Yellow))
-    .block(
-        Block::default()
-            .title("Current Format")
-            .borders(Borders::ALL),
-    );
-
-    // Render the current format widget
-    f.render_widget(current_format_paragraph, layout[0]);
-
-    f.render_widget(list, layout[1]);
-
-    let instructions_paragraph = Paragraph::new(Text::raw(
-        "Use 'j' to move down, 'k' to move up in the list. Use 'h' and 'l' to switch between formats",
-    ))
-    .style(Style::default().fg(Color::Blue))
-    .block(Block::default().title("Instructions").borders(Borders::ALL));
-
-    f.render_widget(instructions_paragraph, layout[2]);
-
-    let (msg, style) = match app.input_mode {
-        InputMode::Normal => (
-            vec![
-                "Press ".into(),
-                "q".bold(),
-                " to exit, ".into(),
-                "e".bold(),
-                " to type in a line number to navigate to.".into(),
-            ],
-            Style::default().add_modifier(Modifier::RAPID_BLINK),
-        ),
-        InputMode::Editing => (
-            vec![
-                "Press ".into(),
-                "Esc".bold(),
-                " to turn off search field, ".into(),
-                "Enter".bold(),
-                "to go to the line number".into(),
-            ],
-            Style::default(),
-        ),
-    };
-    let mut text = Text::from(Line::from(msg));
-    text.patch_style(style);
-    let help_message = Paragraph::new(text);
-    f.render_widget(help_message, layout[4]);
-
-    let input = Paragraph::new(app.input.as_str())
-        .style(match app.input_mode {
-            InputMode::Normal => Style::default(),
-            InputMode::Editing => Style::default().fg(Color::Yellow),
-        })
-        .block(Block::default().borders(Borders::ALL).title("Line number"));
-    f.render_widget(input, layout[3]);
-    match app.input_mode {
-        InputMode::Normal =>
-            // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
-            {}
-
-        InputMode::Editing => {
-            // Make the cursor visible and ask ratatui to put it at the specified coordinates after
-            // rendering
-            f.set_cursor(
-                // Draw the cursor at the current position in the input field.
-                // This position is can be controlled via the left and right arrow key
-                layout[3].x + app.cursor_position as u16 + 1,
-                // Move one line down, from the border to the input line
-                layout[3].y + 1,
-            )
-        }
-    }
-}
 
 
 
@@ -207,4 +95,128 @@ pub fn generate_ui(config: Config) -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+
+
+
+
+
+
+
+// Function to create the list of converted values
+fn create_converted_values_list(app: &mut App) -> List<'static> {
+    let converted_values = match app.current_format {
+        Format::Utf8 => create_display_list(&app.converted_binary_to_utf8.clone(), app),
+        Format::Uint32 => create_display_list(&app.converted_numbers.clone(), app),
+    };
+
+    List::new(converted_values)
+        .block(
+            Block::default()
+                .title("Converted binary values")
+                .borders(Borders::ALL),
+        )
+        .style(Style::default().fg(Color::Green))
+        .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
+        .highlight_symbol(">>")
+        .repeat_highlight_symbol(true)
+}
+
+// Function to create the current format paragraph
+fn create_current_format_paragraph(app: &App) -> Paragraph<'static> {
+    Paragraph::new(Text::raw(format!(
+        "Current Format: {:?}",
+        app.current_format
+    )))
+    .style(Style::default().fg(Color::Yellow))
+    .block(
+        Block::default()
+            .title("Current Format")
+            .borders(Borders::ALL),
+    )
+}
+
+// Function to create the instructions paragraph
+fn create_instructions_paragraph() -> Paragraph<'static> {
+    Paragraph::new(Text::raw(
+        "Use 'j' to move down, 'k' to move up in the list. Use 'h' and 'l' to switch between formats",
+    ))
+    .style(Style::default().fg(Color::Blue))
+    .block(Block::default().title("Instructions").borders(Borders::ALL))
+}
+
+// Function to create the help message
+fn create_help_message(app: &App) -> Paragraph<'static> {
+    let (msg, style) = match app.input_mode {
+        InputMode::Normal => (
+            vec![
+                "Press ".into(),
+                "q".bold(),
+                " to exit, ".into(),
+                "e".bold(),
+                " to type in a line number to navigate to.".into(),
+            ],
+            Style::default().add_modifier(Modifier::RAPID_BLINK),
+        ),
+        InputMode::Editing => (
+            vec![
+                "Press ".into(),
+                "Esc".bold(),
+                " to turn off search field, ".into(),
+                "Enter".bold(),
+                "to go to the line number".into(),
+            ],
+            Style::default(),
+        ),
+    };
+    let mut text = Text::from(Line::from(msg));
+    text.patch_style(style);
+    Paragraph::new(text)
+}
+
+// Function to create the input paragraph
+fn create_input_paragraph(app: &mut App) -> Paragraph<> {
+    Paragraph::new(app.input.as_str())
+        .style(match app.input_mode {
+            InputMode::Normal => Style::default(),
+            InputMode::Editing => Style::default().fg(Color::Yellow),
+        })
+        .block(Block::default().borders(Borders::ALL).title("Line number"))
+}
+
+// Refactored ui function
+fn ui(app: &mut App, f: &mut Frame) {
+    // Define constraints and layout
+    let constraints = [
+        Constraint::Percentage(5),
+        Constraint::Percentage(50),
+        Constraint::Percentage(5),
+        Constraint::Percentage(5),
+        Constraint::Percentage(5),
+        Constraint::Percentage(5),
+        Constraint::Percentage(5),
+    ];
+    let layout = Layout::default()
+        .constraints(constraints.as_ref())
+        .split(f.size());
+
+    // Create widgets
+    let list = create_converted_values_list(app);
+    let current_format_paragraph = create_current_format_paragraph(app);
+    let instructions_paragraph = create_instructions_paragraph();
+    let help_message = create_help_message(app);
+    let input = create_input_paragraph(app);
+
+    // Render widgets
+    f.render_widget(current_format_paragraph, layout[0]);
+    f.render_widget(list, layout[1]);
+    f.render_widget(instructions_paragraph, layout[2]);
+    f.render_widget(help_message, layout[4]);
+    f.render_widget(input, layout[3]);
+
+    // Handle cursor for input mode
+    if let InputMode::Editing = app.input_mode {
+        f.set_cursor(layout[3].x + app.cursor_position as u16 + 1, layout[3].y + 1)
+    }
 }
