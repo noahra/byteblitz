237244f1d7455e9863a034d89d66d9cae29f2385
noahra <noah.rahimzadagan@protonmail.com>
2024-01-30 22:26:16 +0100

    new commits

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,10 @@
 use std::env;
 
+use crate::enums::format::Format;
+
 pub struct Config {
     pub file_path: String,
+    pub format: Format,
     pub little_endianess: bool,
 }
 impl Config {
@@ -11,10 +14,25 @@ impl Config {
         }
 
         let little_endianess = env::var("LITTLE_ENDIAN").is_ok();
+        let mut format = Format::Hex;
+        let file_path: String;
 
-        let file_path = args[1].clone();
+        if args.len() == 3 {
+            file_path = args[1].clone();
+        } else if args.len() > 3 {
+            file_path = args[2].clone();
+            format = match &args[1] {
+                "-hex" => Format::Hex,
+                "-int8" => Format::Int8,
+                "-uint8" => Format::Uint8,
+                // ... handle other formats
+                _ => return Err("invalid format"),
+            };
+        }
+
         Ok(Config {
             file_path,
+            format,
             little_endianess,
         })
     }
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::process;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     if let Err(e) = byteblitz::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
     Ok(())
