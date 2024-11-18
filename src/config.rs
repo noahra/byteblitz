b45a6ef67d8d39a9a3f5e63e01c1d4ae54ab981a
imperivox <imperivox@gmail.com>
2024-11-18 17:24:11 +0100

    config.rs
    -h or --help feature added into config

--- src/config.rs
+++ src/config.rs
@@ -3,9 +3,18 @@ use std::env;
 pub struct Config {
     pub file_path: String,
     pub little_endianess: bool,
+    pub show_help: bool,
 }
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.contains(&"-h".to_string()) || args.contains(&"--help".to_string()) {
+            return Ok(Config {
+                file_path: String::new(),
+                little_endianess: false,
+                show_help: true,
+            });
+        }
+
         if args.len() < 2 {
             return Err("not enough arguments");
         }
@@ -16,6 +25,7 @@ impl Config {
         Ok(Config {
             file_path,
             little_endianess,
+            show_help: false,
         })
     }
 }
